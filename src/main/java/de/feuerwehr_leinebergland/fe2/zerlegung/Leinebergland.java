faf28f8fe5d80c400f8f91126548f8c3b7829646
Lukas Kuntze <privat@lukas-kuntze.de>
2022-02-07 22:56:10 +0100

    Initial commit

---
+++ src/main/java/de/feuerwehr_leinebergland/fe2/zerlegung/Leinebergland.java
@@ -0,0 +1,14 @@
+package de.feuerwehr_leinebergland.fe2.zerlegung;
+
+import de.alamos.fe2.external.interfaces.IAlarmExtractor;
+
+import java.util.Map;
+
+public class Leinebergland implements IAlarmExtractor
+{
+	@Override
+	public Map<String, String> extract(final String s)
+	{
+		return null;
+	}
+}
